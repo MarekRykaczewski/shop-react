a8150d36f9c028df85660c3594412771f24fd2c1
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-12 16:41:17 +0100

    add Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="Nav">
+        <ul>
+            <li>Home</li>
+            <li>Shop</li>
+        </ul>
+    </div>
+  );
+}
+
+export default Nav;
