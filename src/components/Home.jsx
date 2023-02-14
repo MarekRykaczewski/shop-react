bd340f108db5f0069c081b76a51ed504df5fed39
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-14 20:07:18 +0100

    change component return

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function Home() {
   return (
     <div className="home">
-        <div> Home </div>
+      <div className="main">
+        <div className="main--text">
+          <span className="main--title"> iPhone 9 Pro </span>
+          <span className="main--subtitle"> Beyond, Pro </span>
+        </div>
+          <img className="main-image" alt="phone" src="https://images.unsplash.com/photo-1592890288564-76628a30a657?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"></img>
+        </div>
     </div>
   );
 }
