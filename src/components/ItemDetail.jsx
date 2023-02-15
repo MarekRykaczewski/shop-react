7df6a61fde015b0c1ee9cbca49ce672310d904d0
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-15 16:13:34 +0100

    change ItemDetail and styling

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -12,7 +12,10 @@ function ItemDetail() {
 
   return (
     <div className="item-detail">
-        <div> {item.brand} {item.title}  </div>
+        <div className="item-title"> {item.brand} {item.title}  </div>
+        <span className="item-price"> ${item.price }</span>
+        <img className="item-image" src="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="phone" />
+        <button className="item-button"> Add </button>
     </div>
   );
 }
