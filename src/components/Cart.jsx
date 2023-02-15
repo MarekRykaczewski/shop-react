e0d4f40123195cd26d6e233abc8953a6f1840889
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-15 20:41:57 +0100

    add elements to Cart return

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,6 +7,12 @@ function Cart(props) {
       <img className="cart-image" src="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="phone" />
       {item.brand} {item.title}
       <span> ${item.price} </span>
+      <span> Quantity: {item.quantity} </span>
+      <div> 
+      <button>Add</button>
+      <button>Sub</button>
+      </div>
+
     </div>
 
   ))
