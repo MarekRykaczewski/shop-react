e7bb584437d9c8594cd72a443ee0751c610785f2
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-15 16:23:17 +0100

    implement cart state

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import Cart from './components/Cart';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import data from "./data"
 import ItemDetail from './components/ItemDetail';
+import React from 'react';
 
 const items = data
 
 function App() {
+
+  const [cart, setCart] = React.useState([])
+
+  function addToCart(item) {
+    setCart(prevState => [...prevState, item])
+  }
+
+  console.log(cart)
+
   return (
     <Router>
       <div className="App">
@@ -18,7 +28,7 @@ function App() {
           <Route path ="/" element={<Home/>} />
           <Route path ="/shop" element={<Shop data={items}/>} />
           <Route path ='/cart' element={<Cart/>} />
-          <Route path="/shop/:id" element={<ItemDetail/>} />
+          <Route path="/shop/:id" element={<ItemDetail handleClick={addToCart}/>} />
         </Routes>
       </div>
     </Router>
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import data from "../data";
 
-function ItemDetail() {
+function ItemDetail(props) {
 
     const items = data
 
@@ -15,7 +15,7 @@ function ItemDetail() {
         <div className="item-title"> {item.brand} {item.title}  </div>
         <span className="item-price"> ${item.price }</span>
         <img className="item-image" src="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="phone" />
-        <button className="item-button"> Add </button>
+        <button onClick={() => props.handleClick(item)} className="item-button"> Add </button>
     </div>
   );
 }
